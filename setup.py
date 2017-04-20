b2b8d6d83650b7ad027b0419db5d75272cc82643
Eric Douglass <edouglass@lbl.gov>
2017-04-20 13:48:54 -0700

    adding download_url in setup and fixing quote typo

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="yamldown",
-    version="0.1.0",
+    version="0.1.1",
     packages=["yamldown"],
     author="edouglass",
     author_email="edouglass@lbl.gov",
     url="https://github.com/dougli1sqrd/yamldown"
-    description="Python library for loading and dumping "yamldown" (markdown with embedded yaml) files."
+    download_url="https://github.com/dougli1sqrd/yamldown/archive/0.1.1.tar.gz"
+    description="Python library for loading and dumping \"yamldown\" (markdown with embedded yaml) files."
     long_description=open("README.md").read()
     keywords=["yaml", "markdown"]
     install_requires=[

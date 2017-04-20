7eabd2631d2bb168a86ccf85da85dc51cbe11bfb
Eric Douglass <edouglass@lbl.gov>
2017-04-20 13:36:01 -0700

    removing text readme, add setup.cfg

--- setup.py
+++ setup.py
@@ -3,12 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="yamldown",
     version="0.1.0",
+    packages=["yamldown"],
     author="edouglass",
     author_email="edouglass@lbl.gov",
-    packages=find_packages(),
-    include_package_data=True,
+    url="https://github.com/dougli1sqrd/yamldown"
+    description="Python library for loading and dumping "yamldown" (markdown with embedded yaml) files."
+    long_description=open("README.md").read()
+    keywords=["yaml", "markdown"]
     install_requires=[
         "pyYaml==3.12",
     ],
-    long_description=open("README.md").read()
 )

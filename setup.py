9b394cd1f63da21c02ce1fb11e0944aeb22297e1
Eric Douglass <edouglass@lbl.gov>
2019-01-31 10:30:37 -0800

    tying yaml version to 4b2 instead

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="yamldown",
-    version="0.1.5",
+    version="0.1.6",
     packages=["yamldown"],
     author="edouglass",
     author_email="edouglass@lbl.gov",
     url="https://github.com/dougli1sqrd/yamldown",
-    download_url="https://github.com/dougli1sqrd/yamldown/archive/0.1.5.tar.gz",
+    download_url="https://github.com/dougli1sqrd/yamldown/archive/0.1.6.tar.gz",
     description="Python library for loading and dumping \"yamldown\" (markdown with embedded yaml) files.",
     long_description=open("README.md").read(),
     keywords=["yaml", "markdown"],
     install_requires=[
-        "pyYaml>3.13",
+        "pyYaml==4.2b4",
     ],
 )

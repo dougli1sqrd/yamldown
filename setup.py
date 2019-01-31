e97e04746578cce0709f6c00f14cb81a273c7b04
Eric Douglass <edouglass@lbl.gov>
2019-01-31 10:10:14 -0800

    updating tag info

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="yamldown",
-    version="0.1.4",
+    version="0.1.5",
     packages=["yamldown"],
     author="edouglass",
     author_email="edouglass@lbl.gov",
     url="https://github.com/dougli1sqrd/yamldown",
-    download_url="https://github.com/dougli1sqrd/yamldown/archive/0.1.4.tar.gz",
+    download_url="https://github.com/dougli1sqrd/yamldown/archive/0.1.5.tar.gz",
     description="Python library for loading and dumping \"yamldown\" (markdown with embedded yaml) files.",
     long_description=open("README.md").read(),
     keywords=["yaml", "markdown"],

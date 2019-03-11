fb19546f22e1d2475b13e419dca65b5be03ebd87
Eric Douglass <edouglass@lbl.gov>
2019-03-11 15:33:53 -0700

    Upgrade to v0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yamldown",
-    version="0.1.6",
+    version="0.1.7",
     packages=["yamldown"],
     author="edouglass",
     author_email="edouglass@lbl.gov",

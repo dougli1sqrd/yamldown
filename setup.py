07a3943ac38954b2e37f8ac57ccfe0ecaaf0f560
Eric Douglass <edouglass@lbl.gov>
2019-03-25 09:45:48 -0700

    Upgrade to v0.1.8

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="yamldown",
-    version="0.1.7",
+    version="0.1.8",
     packages=["yamldown"],
     author="edouglass",
     author_email="edouglass@lbl.gov",
     url="https://github.com/dougli1sqrd/yamldown",
-    download_url="https://github.com/dougli1sqrd/yamldown/archive/0.1.6.tar.gz",
+    download_url="https://github.com/dougli1sqrd/yamldown/archive/0.1.8.tar.gz",
     description="Python library for loading and dumping \"yamldown\" (markdown with embedded yaml) files.",
     long_description=open("README.md").read(),
     keywords=["yaml", "markdown"],
     install_requires=[
-        "pyYaml==5.1b3",
+        "pyYaml",
     ],
 )
